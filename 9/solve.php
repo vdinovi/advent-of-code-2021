2a9d2aa5ed8384269dd1a7913b553893505db088
vdinovi <vito.dinovi@gmail.com>
2021-12-17 05:38:16 -0800

    9.2

--- 9/solve.php
+++ 9/solve.php
@@ -12,7 +12,9 @@ while ($line = fgets(STDIN)) {
 $height = sizeof($grid);
 $width = sizeof($grid[0]);
 
-$risk_levels = array();
+$visited = array();
+$product_of_basin_sizes = 1;
+$basin_sizes = array();
 for ($y = 0; $y < $height; $y++) {
     for ($x = 0; $x < $width; $x++) {
         $h = $grid[$y][$x];
@@ -20,9 +22,31 @@ for ($y = 0; $y < $height; $y++) {
         if ($x < $width - 1  && $grid[$y][$x + 1] <= $h) { continue; }
         if ($y < $height - 1 && $grid[$y + 1][$x] <= $h) { continue; }
         if ($x > 0           && $grid[$y][$x - 1] <= $h) { continue; }
-        array_push($risk_levels, $h + 1);
+
+        $size = 0;
+        $basin_points = array(array($y, $x));
+        while (sizeof($basin_points) > 0) {
+            $point = array_shift($basin_points);
+            $y1 = $point[0];
+            $x1 = $point[1];
+            if (isset($visited[sprintf("%d,%d", $y1, $x1)])) { continue; }
+
+            $size += 1;
+            $visited[sprintf("%d,%d", $y1, $x1)] = true;
+            if ($y1 > 0 && $grid[$y1 - 1][$x1] > $h && $grid[$y1 - 1][$x1] < 9) { array_push($basin_points, array($y1 - 1, $x1)); }
+            if ($x1 < $width - 1  && $grid[$y1][$x1 + 1] > $h && $grid[$y1][$x1 + 1] < 9) { array_push($basin_points, array($y1, $x1 + 1)); }
+            if ($y1 < $height - 1 && $grid[$y1 + 1][$x1] > $h && $grid[$y1 + 1][$x1] < 9) { array_push($basin_points, array($y1 + 1, $x1));  }
+            if ($x1 > 0 && $grid[$y1][$x1 - 1] > $h && $grid[$y1][$x1 - 1] < 9) { array_push($basin_points, array($y1, $x1 - 1)); }
+        }
+        array_push($basin_sizes, $size);
     }
 }
 
-printf("%d\n", array_sum($risk_levels));
+rsort($basin_sizes);
+$product_of_largest_basin_sizes = 1;
+for ($i = 0; $i < 3; $i++) {
+    $product_of_largest_basin_sizes *= $basin_sizes[$i];
+
+}
+printf("%d\n", $product_of_largest_basin_sizes);
 ?>
