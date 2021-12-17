518d218882b3c43f67eee5c022ee04191ba009a0
vdinovi <vito.dinovi@gmail.com>
2021-12-17 04:45:58 -0800

    9.1

--- 9/solve.php
+++ 9/solve.php
@@ -12,19 +12,17 @@ while ($line = fgets(STDIN)) {
 $height = sizeof($grid);
 $width = sizeof($grid[0]);
 
-//print_r($grid);
 $risk_levels = array();
 for ($y = 0; $y < $height; $y++) {
     for ($x = 0; $x < $width; $x++) {
         $h = $grid[$y][$x];
-        if ($y > 0           && $grid[$y - 1][$x] < $h) { continue; }
-        if ($x < $width - 1  && $grid[$y][$x + 1] < $h) { continue; }
-        if ($y < $height - 1 && $grid[$y + 1][$x] < $h) { continue; }
-        if ($x > 0           && $grid[$y][$x - 1] < $h) { continue; }
+        if ($y > 0           && $grid[$y - 1][$x] <= $h) { continue; }
+        if ($x < $width - 1  && $grid[$y][$x + 1] <= $h) { continue; }
+        if ($y < $height - 1 && $grid[$y + 1][$x] <= $h) { continue; }
+        if ($x > 0           && $grid[$y][$x - 1] <= $h) { continue; }
         array_push($risk_levels, $h + 1);
     }
 }
 
-//print_r($risk_levels);
 printf("%d\n", array_sum($risk_levels));
 ?>
