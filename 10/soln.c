4baf2ad905805ad85f725662e82aacb138a2addd
vdinovi <vito.dinovi@gmail.com>
2021-12-20 13:44:09 -0800

    10.2

--- 10/soln.c
+++ 10/soln.c
@@ -24,6 +24,13 @@ static const size_t scores[4] = {
     25137
 };
 
+static const size_t costs[4] = {
+    1,
+    2,
+    3,
+    4
+};
+
 static inline char partner(const char ch) {
     for (int index = 0; index < sizeof(pairs); index++) {
         if (ch == pairs[index][0]) { return pairs[index][1]; }
@@ -93,14 +100,45 @@ static size_t find_error(const char* line)
     return 0;
 }
 
+static size_t fix_incomplete(const char* line)
+{
+    char buf[1024] = {0};
+    char *ptr = buf;
+    size_t num_chars = strlen(line);
+
+    for (int i = 0; i < num_chars; i++) {
+        char ch = line[i];
+        int dir = direction(ch);
+        if (dir > 0) {
+            *ptr++ = ch;
+        } else if (dir < 0 && ptr > buf) {
+            ptr--;
+            char expected = partner(*ptr);
+            if (expected != ch) {
+                //printf("%s - Expected %c, but found %c instead.\n", line, expected, ch);
+                return 0;
+            }
+        }
+    }
+
+    size_t cost = 0;
+    while (--ptr >= buf) {
+        cost *= 5;
+        cost += costs[index_of(*ptr)];
+    }
+    return cost;
+}
+
 int main(int argc, char **argv)
 {
-    size_t error = 0;
+    size_t cost = 0;
     char line[256 + 1];
     while (fgets(line, 256, stdin)) {
         line[strlen(line) - 1] = 0;
-        error += find_error(line);
+        cost = fix_incomplete(line);
+        if (cost > 0) {
+            printf("%zu\n", cost);
+        }
     }
-    printf("%zu\n", error);
     return 0;
 }
